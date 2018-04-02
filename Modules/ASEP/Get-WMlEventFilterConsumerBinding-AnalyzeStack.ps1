79e87ccdca3d71b7f2409b9446b519da60f69d50
hunter_01@WAYNE.CORP <hunter_01@WAYNE.CORP>
2018-04-02 11:58:01 +0300

    mods

--- Modules/ASEP/Get-WMlEventFilterConsumerBinding-AnalyzeStack.ps1
+++ Modules/ASEP/Get-WMlEventFilterConsumerBinding-AnalyzeStack.ps1
@@ -1,4 +1,4 @@
-﻿if (-Not (Test-Path -Path "*WMIFilterConsumerBinding.csv")) {
+﻿if (-Not (Test-Path -Path "*WMIEventFilterConsumerBinding.csv")) {
     return
 }
 
@@ -9,4 +9,4 @@ if (-Not (Get-Command logparser.exe)) {
 } 
 
 Write-Host Running $(Split-Path $PSCommandPath -Leaf)
-& logparser /q:on "select count(*) as ct,filter,consumer from *WMIFilterConsumerBinding.csv group by filter,consumer order by ct"
+& logparser /q:on "select count(*) as ct,filter,consumer from *WMIEventFilterConsumerBinding.csv group by filter,consumer order by ct"
